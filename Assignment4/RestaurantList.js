c3f711a53cb7af5c2f515423019f53fbe2f7318f
NireekshanaAluwala <69748181+NireekshanaAluwala@users.noreply.github.com>
2020-10-12 19:54:49 +0100

    Add files via upload

---
+++ Assignment4/RestaurantList.js
@@ -0,0 +1,55 @@
+var restaurantList= [
+    {
+        name: "The Bombay Canteen",
+        address: "Ground Floor, Process House Kamala Mills, Near Radio Mirchi Office, S>B. Marg, Lower Parel",
+        city: "Mumbai"
+    },
+    {
+        name: "Toast and Tonic",
+        address: "14/1, Wood Street, Ashok Nagar, Brigade Road",
+        city: "Bangalore",
+
+    },
+    {
+        name: "Bomra's",
+        address: "Souza Vaddo, Candolim",
+        city: "Goa",
+    },
+    {
+        name: "Dum-Pukht_ITC Maurya",
+        address: " ITC Maurya, Sardar Patel Marg, Sardar Patel Marg",
+        city: "NewDelhi",
+    },
+    {
+        name: "Wasabi By Morimoto-The Taj Mahal Palace",
+        address: "The Taj Mahal Palace & Tower, Apollo Bunder, Colaba",
+        city: "Mumbai",
+    },
+    {
+        name: "Farzi Cafe",
+        address: " Shop No. 1200 Jubilee Hill Road No. 5 Telengana, Jubilee Hills",
+        city: "Hyderabad",
+    },
+    {
+        name: "Coromandel Cafe",
+        address: " 8, Romain Rolland St, White Town, White Town",
+        city: "Puducherry",
+    },
+    {
+        name: "Tresind",
+        address: "Ground Floor, Inspire BKC, G Block BKC, Patthar Nagar, Bandra Kurla Complex, Bandra Kurla Complex",
+        city: "Mumbai",
+    },
+    {
+        name: "The Salt House",
+        address: "40, 6th Floor, Shakespeare Sarani, Shakespeare Point, Theatre Road",
+        city: "KolKata",
+    },
+    {
+        name: "Savya Rasa",
+        address: " Gera Serenity, Near Petrol Pump, Off North Main Road, Koregaon Park",
+        city: "Pune",
+    }
+   
+]
+restaurantList.map((data) => { return data.name})
